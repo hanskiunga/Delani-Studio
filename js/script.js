348870635a7862efaa9fc2a35121cbbfb629d6db
hans <hakiunga@gmail.com>
2021-04-20 19:12:23 +0300

    modifies portfolio

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
-$(document).ready(function() {
+$(function() {
     $("#design-image").click(function() {
         $("#design-image").slideToggle();
             $("#design").slideToggle();
     });
     $("#design").click(function(){
         $("#design").slideToggle();
-                $("#design-image").slideToggle(); 
+                $("#design-image").slideToggle();
      });
-  
-  
+
+
     $("#development-image").click(function() {
       $("#development-image").toggle();
         $("#development").toggle();
     });
     $("#development").click(function(){
         $("#development-image").slideToggle();
-            $("#development").slideToggle(); 
+            $("#development").slideToggle();
             });
-  
-  
-  
-  
+
+
+
+
     $("#product-image").click(function() {
         $("#product-image").slideToggle();
             $("#product").slideToggle();
@@ -29,6 +29,10 @@ $(document).ready(function() {
         $("#product").slideToggle();
             $("#product-image").fadeIn();
     });
-  
+
+    $(".text").hide();
+      $("image").animate({
+        opacity: 1
+    });
+
   });
-  
