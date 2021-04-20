361edfef774c94d7ba111b15f0c5ae1980fea869
hans <hakiunga@gmail.com>
2021-04-20 16:18:18 +0300

    Add toggle function

--- js/script.js
+++ js/script.js
@@ -1,33 +1,34 @@
-
-// $(document).ready(function(){
-        //function for toggling
-    $("#design-image").click(function(){
-        $("#design-hide").slideToggle();
+$(document).ready(function() {
+    $("#design-image").click(function() {
         $("#design-image").slideToggle();
+            $("#design").slideToggle();
     });
-    $("#design-hide").click(function(){
-        $("#design-hide").slideToggle();
-        $("#design-image").slideToggle();
-    });
-
-    $("#development-image").click(function(){
-        $("#development").slideToggle();
-        $("#development-image").slideToggle();
+    $("#design").click(function(){
+        $("#design").slideToggle();
+                $("#design-image").slideToggle(); 
+     });
+  
+  
+    $("#development-image").click(function() {
+      $("#development-image").toggle();
+        $("#development").toggle();
     });
     $("#development").click(function(){
-        $("#development").slideToggle();
         $("#development-image").slideToggle();
-    });
-    $("#product-image").click(function(){
-        $("#product").slideToggle();
+            $("#development").slideToggle(); 
+            });
+  
+  
+  
+  
+    $("#product-image").click(function() {
         $("#product-image").slideToggle();
+            $("#product").slideToggle();
     });
     $("#product").click(function(){
         $("#product").slideToggle();
-        $("#product-image").slideToggle();
+            $("#product-image").fadeIn();
     });
-
-
-
-
-// });
+  
+  });
+  
