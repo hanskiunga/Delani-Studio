318af214b56ccdf9b33f3a75a350634414e9ddfe
hans <hakiunga@gmail.com>
2021-04-20 11:09:49 +0300

    Add function for toggling what we do section

---
+++ js/script.js
@@ -0,0 +1,33 @@
+
+// $(document).ready(function(){
+        //function for toggling
+    $("#design-image").click(function(){
+        $("#design-hide").slideToggle();
+        $("#design-image").slideToggle();
+    });
+    $("#design-hide").click(function(){
+        $("#design-hide").slideToggle();
+        $("#design-image").slideToggle();
+    });
+
+    $("#development-image").click(function(){
+        $("#development").slideToggle();
+        $("#development-image").slideToggle();
+    });
+    $("#development").click(function(){
+        $("#development").slideToggle();
+        $("#development-image").slideToggle();
+    });
+    $("#product-image").click(function(){
+        $("#product").slideToggle();
+        $("#product-image").slideToggle();
+    });
+    $("#product").click(function(){
+        $("#product").slideToggle();
+        $("#product-image").slideToggle();
+    });
+
+
+
+
+// });
